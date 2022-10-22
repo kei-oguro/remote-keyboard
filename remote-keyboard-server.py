277bb3f68207472854a6bd54843a2d2e5c1c6d36
Kei Oguro <kei.oguro@thebugmakers.com>
2022-10-22 18:27:55 +0900

    httpサーバを立てて、 /send へのリクエストでkeyの値のキーボードイベントを発生させる。
    curl http://localhost:8000/send?key=abc などとすると試せる。

--- remote-keyboard-server.py
+++ remote-keyboard-server.py
@@ -1,9 +1,24 @@
-from time import sleep
-from pynput.keyboard import Controller, Key
-from sys import argv
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import parse_qs, urlparse
+from pynput.keyboard import Controller
 
 keyboard = Controller()
-with keyboard.pressed(Key.alt):
-    keyboard.tap(Key.tab)
-sleep(1)
-keyboard.type(argv[1] if len(argv) > 1 else "abcあいう")
+
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.end_headers()
+        parsed = urlparse(self.path)
+        if parsed.path == "/send":
+            self.send(parse_qs(parsed.query))
+
+    def send(self, query):
+        if keys := query.get("key"):
+            for key in keys:
+                keyboard.type(key)
+
+
+HTTPServer(("", 8000), MyServer).serve_forever()
