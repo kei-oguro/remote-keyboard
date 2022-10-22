65a318356b580fe1ba4008b98bd730a0c7ff8381
Kei Oguro <kei.oguro@thebugmakers.com>
2022-10-22 20:51:07 +0900

    / ならクライアントページを送信、 /send ならキーイベントの処理、存在しないurlなら404を返すようにした。
    README.mdのメンテ。将来の予定も書いた。
    /welcome の受け口だけ作った。

--- README.md
+++ README.md
@@ -1,6 +1,41 @@
 # remote-keyboard
-windows server uses SetInput() and Javascript client sends key-code or unicode string.
+python http server and Javascript client sends key-code or unicode string.
+
+## Server
+
+python script boots the http server.
+Server waits requests.
+Requests contains keycodes and shift, ctrl, alt, meta keys status.
+
+ex)
+```
+curl http://localhost:8000/send?key=a&shift=1
+```
+
+(not implemented) Server deliver client page.
+- Server returns client html page, if path is empty.
+- If path is `webcome`, server returns QR code .png image to input httpd server address, like `http://192.168.1.2/`.
+  - You display this page on server machine and read QR code to open the page on client machine (e.g. smart phone).
+
+## Client
+
+The single html page sends http request to server.
+Requests contains keycodes and meta key status.
+If you want, use `curl` or similar tools to send request.
 
 ## Dependency
 
 - pynput
+
+## Install
+
+1. Clone this project or download.
+2. Install python3.
+3. Install dependencies. Like ... `pip install pynput`. Add `--user` is you need.
+
+## Run
+
+1. Run script. `python3 remote-keyboard-server.py`
+2. (not implemented) Access `http://localhost:8000/` by browser on same machine. Then QR code image will be displayed.
+3. Read QR code by phone camera to open client page.
+
--- remote-keyboard-server.py
+++ remote-keyboard-server.py
@@ -7,18 +7,43 @@ keyboard = Controller()
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.send_header("Access-Control-Allow-Origin", "*")
-        self.end_headers()
         parsed = urlparse(self.path)
         if parsed.path == "/send":
             self.send(parse_qs(parsed.query))
+        elif parsed.path == "/welcome":
+            self.displayAddress()
+        elif parsed.path == "/":
+            self.displayClientPage()
+        else:
+            self.send_error(404, f"Unknown resource address {parsed.path}")
 
-    def send(self, query):
+    def send(self, query) -> None:
+        self.sendHeader("text/plain")
         if keys := query.get("key"):
             for key in keys:
                 keyboard.type(key)
+                print(key)
+
+    def displayAddress(self) -> None:
+        self.send_error(404, "not implemented, yet.")
+        # self.sendHeader("image/jpeg")
+        # todo: convert address into QR code.
+
+    def displayClientPage(self) -> None:
+        self.sendHeader("text/html")
+        self.wfile.write(self.GetClientPage())
+
+    def GetClientPage(self) -> bytes:
+        if not hasattr(self, "clientPage") or self.clientPage is None:
+            with open("remote-keyboard-client.html", "rb") as f:
+                self.clientPage = f.read()
+        return self.clientPage
+
+    def sendHeader(self, contentType: str) -> None:
+        self.send_response(200)
+        self.send_header("Content-type", contentType)
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.end_headers()
 
 
 HTTPServer(("", 8000), MyServer).serve_forever()
